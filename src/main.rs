3c63b8ac3fa3726b9bce7cfbf764d9dd425bb8a4
duggurd <alexanderhaugerud@gmail.com>
2024-03-24 17:44:41 +0100

    +1

--- src/main.rs
+++ src/main.rs
@@ -17,8 +17,9 @@ fn main() {
                 let mut buf = String::new();
 
                 println!("accepted new connection");
-                for _ in 0..=1 {
-                    // _stream.read_to_string(&mut buf).unwrap();
+
+                for _ in 0..=2 {
+                    _stream.read_to_string(&mut buf).unwrap();
                     _stream.write(b"+PONG\r\n").unwrap();
                 }
             }
